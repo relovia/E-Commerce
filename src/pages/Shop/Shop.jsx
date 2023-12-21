942805dd1d1778d1e11119e39d5feb7980115a7a
Yunus Emre <emrecivan06x@gmail.com>
2023-12-21 20:09:30 +0300

    updated shop

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,8 +1,14 @@
+import Products from "../../shopData.json";
+import ProductCard from "../../components/productCard/ProductCard";
+
 const Shop = () => {
   return (
     <>
-      <div>
-        <h1>Her kategoriden 5 ürün burada listelenecek</h1>
+      <h1 className="text-5xl p-4">Hats</h1>
+      <div className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 gap-y-3">
+        {Products.map((Product) => (
+          <ProductCard key={Product.id} Product={Product} />
+        ))}
       </div>
     </>
   );
