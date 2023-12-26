804f98deb413c71314e31a3fe417b5d01f57eff4
Yunus Emre <emrecivan06x@gmail.com>
2023-12-26 06:09:35 +0300

    added new reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import signUpReducer from "./signSlice";
 import userReducer from "./userSlice";
+import cartReducer from "./cartSlice";
 
 export const store = configureStore({
   reducer: {
     signUp: signUpReducer,
     user: userReducer,
+    cart: cartReducer,
   },
 });
