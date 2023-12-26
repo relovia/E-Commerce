c38ce6a913ada9e18396ad3521eb23e1d119cf79
Yunus Emre <emrecivan06x@gmail.com>
2023-12-26 20:50:09 +0300

    navigate to hats

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,10 +1,13 @@
 import products from "../../shopData.json";
 import ProductCard from "../../components/productCard/ProductCard";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
   return (
     <>
-      <h1 className="text-5xl p-4">Hats</h1>
+      <Link to={"./hats"}>
+        <h1 className="text-5xl cursor-pointer p-4">Hats</h1>
+      </Link>
       <div className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 gap-y-3">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
