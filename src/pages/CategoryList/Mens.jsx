01cefdb1405d548f9ce85a6fe5075b15d6ec6298
Yunus Emre <emrecivan06x@gmail.com>
2023-12-26 06:08:50 +0300

    deleted img

--- src/pages/CategoryList/Mens.jsx
+++
@@ -1,9 +0,0 @@
-const Mens = () => {
-  return (
-    <div>
-      <h1>Mens</h1>
-    </div>
-  );
-};
-
-export default Mens;
--- src/pages/CategoryList/Womens.jsx
+++
@@ -1,9 +0,0 @@
-const Womens = () => {
-  return (
-    <div>
-      <h1>Womens</h1>
-    </div>
-  );
-};
-
-export default Womens;
