f85618853e9d51ca950d97ddc377436490a8e105
Yunus Emre <emrecivan06x@gmail.com>
2023-12-26 06:10:18 +0300

    custom media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,14 @@ export default {
         350: "21.875rem",
       },
     },
+    screens: {
+      xs: "475px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
   },
   plugins: [],
 };
