4ea48feb2a64434d8ca1bdc90345288a073cbcc5
Yunus Emre <emrecivan06x@gmail.com>
2023-12-26 20:50:43 +0300

    listin' all hat products

--- src/pages/CategoryList/Hats.jsx
+++ src/pages/CategoryList/Hats.jsx
@@ -1,7 +1,12 @@
+import ProductCard from "../../components/productCard/ProductCard";
+import products from "../../shopData.json";
+
 const Hats = () => {
   return (
-    <div>
-      <h1>Hats</h1>
+    <div className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 gap-y-3">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 };
